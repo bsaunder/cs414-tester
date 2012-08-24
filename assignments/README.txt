e1e805ca14a01c311065457bee3fda96d7d53231
Bryan Saunders <btsaunde@gmail.com>
2012-08-24 19:20:16 -0400

    Initial Test Script

---
+++ assignments/README.txt
@@ -0,0 +1,4 @@
+Place your .jar files for submission here if you want to run JUnit against them. They should be in the format of A[X]-[eID].jar where X is the Assignment Number and eID is your eID.
+
+Example:
+A1-btsaunde.jar
---
+++ test.bat
@@ -0,0 +1,33 @@
+@echo off
+set /p assignmentNumber=What is the Assignment Number (1-99)? 
+set /p eID=What is your eID? 
+
+echo Testing Archive A%assignmentNumber%-%eID%.jar...
+
+echo Making test Directory...
+mkdir test
+
+echo Copying Jar...
+copy assignments\A%assignmentNumber%-%eID%.jar test\test.jar
+
+echo Extracting Test Jar...
+cd test
+jar xvf test.jar
+cd ..\
+
+echo Compiling Source...
+cd test\cs414\a%assignmentNumber%\%eID%
+javac -cp ../../../../libs/junit-4.10.jar;. *.java
+cd ..\..\..\..\
+
+echo Running JUnit...
+java -cp libs\junit-4.10.jar;test\;. org.junit.runner.JUnitCore cs414.a%assignmentNumber%.%eID%.TestAll > results.txt
+
+echo Results:
+cat results.txt
+
+echo Cleaning Up...
+del results.txt
+rmdir /S/Q test
+
+echo Done
